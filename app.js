0948ab99ca900a6cfda12f9821b0c60ca0f1cd54
hanacoon <hanacoon@Hanas-MacBook-Pro-2.local>
2018-12-27 18:28:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//This code is opening a port on the local server and serving some HTML.
+
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
+});
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
